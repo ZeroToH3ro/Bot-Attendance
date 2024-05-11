6034e446c873110ad86814c12a6b824dad986e02
ZeroToH3ro <mtblaser2002@gmail.com>
2024-05-11 08:21:40 +0700

    update csv file

--- location_attendance.rb
+++ location_attendance.rb
@@ -88,7 +88,7 @@ class LocationAttendance
     if message.from.username == PROFESSOR_NAME
       bot.api.send_document(chat_id: message.chat.id, document: Faraday::UploadIO.new(CSV_FILE_PATH, 'text/csv'))
       conn.close
-      File.delete(CSV_FILE_PATH)
+      # File.delete(CSV_FILE_PATH)
     else
       bot.api.send_message(chat_id: message.chat.id, text: "You are not professor. So I can not send CSV FILE to you")
     end
